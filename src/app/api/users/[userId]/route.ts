7a58a9e7b9afcfedceb8f4b1386f736640869966
Jaylaelike <smarkwisai@gmail.com>
2024-02-07 18:43:41 +0700

    fix eslint disable.

--- src/app/api/users/[userId]/route.ts
+++ src/app/api/users/[userId]/route.ts
@@ -7,53 +7,53 @@ interface contextProps {
   };
 }
 
-export async function DELETE(req: Request, context: contextProps) {
-  try {
-    const { params } = context;
+// export async function DELETE(req: Request, context: contextProps) {
+//   try {
+//     const { params } = context;
 
-    await db.post.delete({
-      where: {
-        id: params.userId,
-      },
-    });
+//     await db.post.delete({
+//       where: {
+//         id: params.userId,
+//       },
+//     });
 
-    return new Response(null, { status: 204 });
-  } catch (error) {
-    return NextResponse.json(
-      { message: "could not delete post" },
-      { status: 500 }
-    );
-  }
-}
+//     return new Response(null, { status: 204 });
+//   } catch (error) {
+//     return NextResponse.json(
+//       { message: "could not delete post" },
+//       { status: 500 }
+//     );
+//   }
+// }
 
-export async function PATCH(req: Request, context: contextProps) {
-  try {
-    const { params } = context;
+// export async function PATCH(req: Request, context: contextProps) {
+//   try {
+//     const { params } = context;
 
-    const body = await req.json();
+//     const body = await req.json();
 
-    await db.post.update({
-      where: {
-        id: params.userId,
-      },
-      data: {
-        title: body.title,
-        content: body.content,
-        tagId: body.tagId,
-      },
-    });
+//     await db.post.update({
+//       where: {
+//         id: params.userId,
+//       },
+//       data: {
+//         title: body.title,
+//         content: body.content,
+//         tagId: body.tagId,
+//       },
+//     });
 
-    return NextResponse.json(
-      { message: `update id ${params.userId} success` },
-      { status: 200 }
-    );
-  } catch (error) {
-    return NextResponse.json(
-      { message: "could not update post" },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json(
+//       { message: `update id ${params.userId} success` },
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     return NextResponse.json(
+//       { message: "could not update post" },
+//       { status: 500 }
+//     );
+//   }
+// }
 
 
 export async function GET(req: Request, context: contextProps){
