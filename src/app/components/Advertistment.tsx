080d6ac5f1ec45fded0ca74d6fc6fca7fd5196a7
Jaylaelike <smarkwisai@gmail.com>
2024-02-09 16:20:13 +0700

    Add Advertisment carousel roller intervel 5 sec.

--- src/app/components/Advertistment.tsx
+++ src/app/components/Advertistment.tsx
@@ -1,50 +1,101 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+
+import { Circle } from "lucide-react";
 
 function Advertistment() {
+  //create roll loop interval for the carousel items using use useEffect and usestate
+
+  const [currentItem, setCurrentItem] = useState(1);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentItem((prevItem) => (prevItem % 4) + 1);
+    }, 5000); // Change item every 3 seconds
+
+    return () => clearInterval(interval); // Clean up on unmount
+  }, []);
+
   return (
     <>
       <div className="flex flex-col items-center justify-center w-full ">
-        <div className="card w-full bg-base-96 shadow-xl">
-          <div className="carousel w-full">
-            <div id="item1" className="carousel-item w-full">
-              <img
-                src="https://res.cloudinary.com/satjay/image/upload/c_fill,g_auto,h_250,w_970/b_rgb:000000,e_gradient_fade,y_-0.50/c_scale,co_rgb:ffffff,fl_relative,l_text:montserrat_25_style_light_align_center:Shop%20Now,w_0.5,y_0.18/v1707388870/wpajf424bbjlqwh0yqfg.png"
-                className="w-full"
-              />
-            </div>
-            <div id="item2" className="carousel-item w-full">
-              <img
-                src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
-                className="w-full"
-              />
-            </div>
-            <div id="item3" className="carousel-item w-full">
-              <img
-                src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg"
-                className="w-full"
-              />
-            </div>
-            <div id="item4" className="carousel-item w-full">
-              <img
-                src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg"
-                className="w-full"
-              />
-            </div>
+        <div className="carousel rounded-box w-full">
+          {/* Show the carousel items based on the currentItem state */}
+          <div
+            id="item1"
+            className={`carousel-item w-full slide-right ${
+              currentItem === 1 ? "block" : "hidden"
+            }`}
+          >
+            <img
+              src="https://res.cloudinary.com/satjay/image/upload/v1707463991/ostxspdeakcsbmuzusrl.png"
+              className="w-full"
+            />
           </div>
-          <div className="flex justify-center w-full py-2 gap-2">
-            <a href="#item1" className="btn btn-xs">
-              1
-            </a>
-            <a href="#item2" className="btn btn-xs">
-              2
-            </a>
-            <a href="#item3" className="btn btn-xs">
-              3
-            </a>
-            <a href="#item4" className="btn btn-xs">
-              4
-            </a>
+          <div
+            id="item2"
+            className={`carousel-item w-full slide-right ${
+              currentItem === 2 ? "block" : "hidden"
+            }`}
+          >
+            <img
+              src="https://res.cloudinary.com/satjay/image/upload/c_fill,g_auto,h_250,w_970/b_rgb:000000,e_gradient_fade,y_-0.50/c_scale,co_rgb:ffffff,fl_relative,l_text:montserrat_25_style_light_align_center:Shop%20Now,w_0.5,y_0.18/v1707388870/wpajf424bbjlqwh0yqfg.png"
+              className="w-full"
+            />
           </div>
+          <div
+            id="item3"
+            className={`carousel-item w-full slide-right ${
+              currentItem === 3 ? "block" : "hidden"
+            }`}
+          >
+            <img
+              src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg"
+              className="w-full"
+            />
+          </div>
+
+          <div
+            id="item4"
+            className={`carousel-item w-full slide-right ${
+              currentItem === 4 ? "block" : "hidden"
+            }`}
+          >
+            <img
+              src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg"
+              className="w-full"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center w-full py-2 gap-2">
+        <div
+          className={`btn btn-xs ${currentItem === 1 ? "active" : ""}`}
+          onClick={() => setCurrentItem(1)}
+        >
+          <a className="btn btn-xs">
+            <Circle size={10} />
+          </a>
+        </div>
+
+        <div
+          className={`btn btn-xs ${currentItem === 2 ? "active" : ""}`}
+          onClick={() => setCurrentItem(2)}
+        >
+          {" "}
+          <a className="btn btn-xs">
+            <Circle size={10} />
+          </a>
+        </div>
+        <div
+          className={`btn btn-xs ${currentItem === 3 ? "active" : ""}`}
+          onClick={() => setCurrentItem(3)}
+        >
+          {" "}
+          <a className="btn btn-xs">
+            <Circle size={10} />
+          </a>
         </div>
       </div>
     </>
